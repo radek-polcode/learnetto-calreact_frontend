7a6af6fc739ced3eed84cb3f304b07ab2aded00e
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-11-23 09:58:23 +0100

    Port files from react_on_rails to react app

---
+++ src/components/Appointments.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import AppointmentForm from './AppointmentForm';
+import { AppointmentsList } from './AppointmentsList';
+import update from 'immutability-helper';
+import PropTypes from 'prop-types';
+import $ from 'jquery';
+
+export default class Appointments extends React.Component {
+  static propTypes = {
+    appointments: PropTypes.array.isRequired
+  }
+
+  static defaultProps = {
+    appointments: []
+  }
+
+  constructor (props, railsContext) {
+    super(props)
+    this.state = {
+      appointments: this.props.appointments
+    }
+  }
+
+  addNewAppointment = (appointment) => {
+    const appointments = update(this.state.appointments, { $push: [appointment]});
+    this.setState({
+      appointments: appointments.sort(function(a,b){
+        return new Date(a.appt_time) - new Date(b.appt_time);
+      })
+    });
+  }
+
+  componentDidMount () {
+    if(this.props.match) {
+      $.ajax({
+        type: "GET",
+        url: 'http://localhost:3001/appointments',
+        dataType: "JSON"
+      }).done((data) => {
+        this.setState({appointments: data})
+      })
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <AppointmentForm handleNewAppointment={this.addNewAppointment} />
+        <AppointmentsList appointments={this.state.appointments} />
+      </div>
+    )
+  }
+}
