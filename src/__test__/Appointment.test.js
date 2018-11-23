bcf626c86b709a322ba4e8207be7523b618f7653
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-11-23 14:15:20 +0100

    Add test

---
+++ src/__test__/Appointment.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Appointment from '../components/Appointment';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { shallow, mount } from 'enzyme';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  shallow(<Appointment />);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('render', () => {
+  it('should display the appointment title', () => {
+    const appointment = mount(
+      <Router>
+        <Appointment appointment={
+          {
+            id: 1, 
+            title: 'Team standup meeting', 
+            appt_time: new Date()
+          }
+        } />
+      </Router>
+    );
+
+    const title = <h3>Team standup meeting</h3>;
+    expect(appointment.contains(title)).toEqual(true);
+  })
+
+  it('should display the appointment date', () => {
+    const appointment = mount(
+      <Router>
+        <Appointment appointment={
+          {
+            id: 1, 
+            title: 'Team standup meeting', 
+            appt_time: new Date('04/11/2018, 12:00:00')
+          }
+        } />
+      </Router>
+    );
+
+    const appt_time = <p>April 11 2018, 12:00:00 pm</p>;
+    expect(appointment.contains(appt_time)).toEqual(true);
+  })
+})
